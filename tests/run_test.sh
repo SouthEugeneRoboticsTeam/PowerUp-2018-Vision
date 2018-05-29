580e3e352fc3cb77a1adf5ed5c800eb1db7bbc7a
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-05-29 09:30:18 -0700

    Add tests to Travis

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -3,18 +3,20 @@
 mkdir -p test_results
 
 # Run 2016 test
-python run.py -i tests/images/goal2016.png -o test_results/goal2016.jpg -l 60 100 175 -u 85 250 255
+python ../run.py -i images/goal2016.png -o test_results/goal2016.jpg -l 60 100 175 -u 85 250 255
 result2016=$(cmp --silent tests/images/goal2016.png test_results/goal2016.jpg || echo "1")
 
 # Run 2017 test
-python run.py -i tests/images/goal2017.png -o test_results/goal2017.jpg -l 60 225 225 -u 70 255 255
+python ../run.py -i images/goal2017.png -o test_results/goal2017.jpg -l 60 225 225 -u 70 255 255
 result2017=$(cmp --silent tests/images/goal2016.png test_results/goal2016.jpg || echo "1")
 
-if [ "$result2016$result2017" -ne "" ]
+result=$(echo "$result2016$result2017" | xargs)
+
+if [ "$result" = "" ]
 then
-    echo "Test failed!"
-    exit 1
-else
     echo "Test passed!"
     exit 0
+else
+    echo "Test failed!"
+    exit 1
 fi
